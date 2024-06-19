c5c46d704a723f9b2994ae96fb8f44f9215fd511
AnmolPdy <anmolpdy04@gmail.com>
2024-06-20 03:46:24 +0530

    solved for optimal approach

--- Lec/Array_3_L12/CPP/Q6Rain_water_trap.cpp
+++ Lec/Array_3_L12/CPP/Q6Rain_water_trap.cpp
@@ -1,18 +1,56 @@
 #include <iostream>
 using namespace std;
 
+//most optimal approach
+int optimal(int arr[], int n){
+    int total=0;
+    int i=0;
+    int j=n-1;
+    int left=0;
+    int right=0;
+
+    while(i<=j){
+        left=max(left,arr[i]);
+        right=max(right,arr[j]);
+
+        if(left<right){
+            int watertrapped=left-arr[i];
+            total+=watertrapped;
+            i++;
+        }else{
+            int watertrapped=right-arr[j];
+            total+=watertrapped;
+            j--;
+        }
+    }
+    return total;
+
+}
 
 
 
 
 
+//optimized method
+int optimized(int arr[], int n){
+    int total=0;
 
+    int right[n];
+    right[n-1]=arr[n-1];
+    for(int i=n-2;i>=0;i--){
+        right[i]=max(right[i+1],arr[i]);
+    }
 
+    int left=0;
+    for(int i=0;i<n;i++){
+        left=max(left,arr[i]);
+        int watertrapped=min(left,right[i])-arr[i];
+        total+=watertrapped;
+    }
 
-
-
-
-
+    return total;
+    
+}
 
 
 
@@ -36,9 +74,15 @@ int brute_force(int arr[], int n){
 
         int water_trapped=min(leftblock,rightblock)-arr[i];
         total+=water_trapped;
+
     }
     return total;
 }
+
+
+
+
+
 
 int main(){
     int n;
@@ -50,6 +94,10 @@ int main(){
     }
 
     cout<<brute_force(arr,n);
+
+    cout<<optimized(arr,n);
+
+    cout<<optimal(arr,n);
 
     return 0;
 }
